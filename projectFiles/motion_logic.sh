aebc0d0c556d1b3ed161808b20d7125424f058f1
Rizzberry Pi <rpi@mail.dk>
2024-05-21 14:04:10 +0100

    motion logic working

--- projectFiles/motion_logic.sh
+++ projectFiles/motion_logic.sh
@@ -11,12 +11,13 @@ while true ; do
 	img_paths=$(grep -Rl "" ./ | grep ".jpg" | sort | tail -n 2)
 	json_paths=$(grep -Rl "" ./ | grep ".json" | sort | tail -n 2)
 	res="$(python3 /home/emil/projectFiles/motion_detect.py $(echo $img_paths | tr '\n' ' '))"
+
 	if [ "$res" = "Motion detected" ]; then	
 		img_path=$(echo "$img_paths" | tail -n 1)
-		mkdir -p "/home/emil/$photo_dir$(echo $img_path | grep -Po '\/.+\/')"
-		cp "$img_path" "/home/emil/$photo_dir$(echo $img_path | grep -Po '\/.+')"
+		sudo mkdir -p "/home/emil/$photo_dir$(echo $img_path | grep -Po '\/.+\/')"
+		sudo cp "$img_path" "/home/emil/$photo_dir$(echo $img_path | grep -Po '\/.+')"
 		json_path=$(echo "$json_paths" | tail -n 1) 
-		cp "$json_path" "/home/emil/$photo_dir$(echo $json_path | grep -Po '\/.+')"
+		sudo cp "$json_path" "/home/emil/$photo_dir$(echo $json_path | grep -Po '\/.+')"
 	fi
 	img_path=$(echo "$img_paths" | tail -n 1 | grep -Po '\/.+')
 	json_path=$(echo "$json_paths" | tail -n 1 | grep -Po '\/.+') 
