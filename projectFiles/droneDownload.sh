842e64691531d4d26eb964e9cb03d1a6f017b5d4
sigurd <sigurdjagerlemming@gmail.com>
2024-05-28 16:51:13 +0200

    sql logging added to droneDownload

--- projectFiles/droneDownload.sh
+++ projectFiles/droneDownload.sh
@@ -70,6 +70,16 @@ while true; do
 	    	exitBool="0"
 	    	break
 	    fi
+		# Log wifi signal quality to sql database
+		#mysql --user=root --password=password -e "CREATE DATABASE IF NOT EXISTS logDB"
+		#mysql --user=root --password=password -e "USE logDB; CREATE TABLE IF NOT EXISTS wifistats (time DOUBLE(40, 3), link_quality INT, signal_level INT);"
+		wifi_stats=$(tail /proc/net/wireless)
+		regex=' -*[\d]+'
+		res=($(echo "$wifi_stats" | grep -Po "$regex"))
+		link=$(echo "${res[2]}")
+		level=$(echo "${res[3]}")
+		secs=$(date +%s.%3N)
+		mysql --user=root --password=password -e "INSERT INTO logDB.wifistats (time, link_quality, signal_level) VALUES ('$secs', '$link', '$level');"
 	done
 
 done
