5b3708791229ff7b976b9024bba07643bcf100bf
Philip <phsan20@sdu.student.dk>
2024-05-31 19:15:00 +0200

    fixed 13

--- TestFiles/FR13.sh
+++ TestFiles/FR13.sh
@@ -2,15 +2,16 @@ echo "--------------------------------"
 echo "Test for FR13"
 
 #Drone Script
-echo "Turn on drone download script"
-sudo bash droneDownload.sh
-sleep 2
 echo "Count number of wifilogs"
-var=mysql --user=root --password=password -e "SELECT COUNT(*) FROM logDB.wifistats;" | grep '[0-9]' | sed 's/[^0-9]*//g'
+var="$(mysql --user=root --password=password -e "SELECT COUNT(*) FROM logDB.wifistats;" | grep '[0-9]' | sed 's/[^0-9]*//g')"
+
+echo "Turn on drone download script, ctrl+C once after downloading a few photos"
+sudo bash ../projectFiles/droneDownload.sh
+sleep 2
 sleep 2
 echo "Wait 2 seconds"
 echo "Count number of wifilogs again"
-var2=mysql --user=root --password=password -e "SELECT COUNT(*) FROM logDB.wifistats;" | grep '[0-9]' | sed 's/[^0-9]*//g'
+var2="$(mysql --user=root --password=password -e "SELECT COUNT(*) FROM logDB.wifistats;" | grep '[0-9]' | sed 's/[^0-9]*//g')"
 
 if [ $var2 -gt $var ]; then
     echo "FR13 Passed"
