5bb23658e44160ad38ed4e74d6a6d8c6dc3b5a3b
Rizzberry Pi <rpi@mail.dk>
2024-05-31 17:38:20 +0100

    fixed tests

--- TestFiles/FR11.sh
+++ TestFiles/FR11.sh
@@ -1,17 +1,18 @@
 echo "--------------------------------"
 echo "Test for FR11"
-#Drone Script
-echo "Turn of network card and then turn it back on"
-nmcli d disconnect wlo1
-nmcli d connect wlo1
+read -n 1 -p "run droneDownload.sh and press enter." input
+oldDate="$(date +%Y+%M+%d)"
 
-sudo bash ../projectFiles/droneDownload.sh &
-echo "Run drone download script" #Might have to run seperately if this file gets stuck in loop.
-sleep 3
-ssid_of_connected_wifi=$(iwgetid)
-echo "Slept for 3 seconds on now is getting the SSID of currently connected wifi"
+#set wrong date
+sudo date --set="1999-04-28T15:25:47 BST"
 
-if  [[ $ssid_of_connected_wifi =~ "EMIL-TEAM-19-2.4GHz" ]]; then
+#try to sync
+read -n 1 -p "run droneDownload.sh and press enter." input
+
+newDate="$(date +%Y+%M+%d)"
+
+
+if  [ "$newDate" == "$oldDate" ]; then
     echo "FR11 Passed"
 else
     echo "FR11 Failed"
