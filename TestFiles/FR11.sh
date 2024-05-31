4a34173944faf6f5155958783912f56577be743b
Rizzberry Pi <rpi@mail.dk>
2024-05-31 17:43:55 +0100

    test fixed

--- TestFiles/FR11.sh
+++ TestFiles/FR11.sh
@@ -1,15 +1,15 @@
 echo "--------------------------------"
 echo "Test for FR11"
 read -n 1 -p "run droneDownload.sh and press enter." input
-oldDate="$(date +%Y+%M+%d)"
+oldDate="$(date +%Y+%m+%d)"
 
 #set wrong date
 sudo date --set="1999-04-28T15:25:47 BST"
-
+sleep 10 #wait for ImageUploader to timeout 3 times
 #try to sync
 read -n 1 -p "run droneDownload.sh and press enter." input
 
-newDate="$(date +%Y+%M+%d)"
+newDate="$(date +%Y+%m+%d)"
 
 
 if  [ "$newDate" == "$oldDate" ]; then
