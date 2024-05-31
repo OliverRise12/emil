72d2261de2a331d005c412f54cf66e53d6d0b7f7
Rizzberry Pi <rpi@mail.dk>
2024-05-31 09:58:23 +0100

    fixed another test

--- TestFiles/FR6.sh
+++ TestFiles/FR6.sh
@@ -2,15 +2,15 @@ TRIGGERTOPIC="emil/wildlife_trigger"
 echo "--------------------------------"
 echo "Test for FR6"
 #Find innit number of photos
-cd ../photos
-var=$(ls -1 | wc -l)
+cd /var/www/html/photos
+var=$(find /var/www/html/photos -type f | wc -l) #$(ls -1 | wc -l)
 echo "Initially photos has $var photos"
 
 echo "Publishing to $TRIGGERTOPIC to activate external trigger"
 mosquitto_pub -h localhost -p 1883 -u emil -P emil -t $TRIGGERTOPIC -m "1"
 
-
-varfter=$(ls -1 | wc -l)
+sleep 2
+varfter=$(find /var/www/html/photos -type f | wc -l)
 echo "After having activated the trigger, photos has $varfter photos"
 
 #Check if photos grew by 2 in size
