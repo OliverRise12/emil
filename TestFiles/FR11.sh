846e5446219eb36b04e53835ba1af26eba0e11d3
Philip <phsan20@sdu.student.dk>
2024-05-31 11:27:09 +0200

    another test fixed

--- TestFiles/FR11.sh
+++ TestFiles/FR11.sh
@@ -1,15 +1,16 @@
 echo "--------------------------------"
 echo "Test for FR11"
 echo "Turn of network card and then turn it back on"
-nmcli d disconnect wlp9s0
-nmcli d connect wlp9s0
+nmcli d disconnect wlo1
+nmcli d connect wlo1
 
-sudo bash droneDownload.sh
+sudo bash ../projectFiles/droneDownload.sh &
 echo "Run drone download script" #Might have to run seperately if this file gets stuck in loop.
 sleep 3
-ssid_of_connected_wifi = iwgetid
+ssid_of_connected_wifi=$(iwgetid)
 echo "Slept for 3 seconds on now is getting the SSID of currently connected wifi"
-if [ $(ssid_of_connected_wifi) -eq "EMIL-TEAM-19-2.4GHz" ]; then
+
+if  [[ $ssid_of_connected_wifi =~ "EMIL-TEAM-19-2.4GHz" ]]; then
     echo "FR11 Passed"
 else
     echo "FR11 Failed"
