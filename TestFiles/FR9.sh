d10730fa5934573191e20835ce5e67bcb920b9c7
OliverRise12 <oliverrise@hotmail.com>
2024-05-31 11:12:40 +0200

    fixed test 9

--- TestFiles/FR9.sh
+++ TestFiles/FR9.sh
@@ -1,16 +1,19 @@
+TRIGGERTOPIC="emil/wildlife_trigger"
 echo "--------------------------------"
 echo "Test for FR9"
-#Cd into html folder
-#rm log file
+#count amount of lines in logfile
+var=wc -l /var/www/html/EventLog.txt
 
+cd ../projektFiles/take_photo.sh
 #do something loggable
-
+mosquitto_pub -h localhost -p 1883 -u emil -P emil -t $TRIGGERTOPIC -m "1"
+sleep(2)
 # assert presence of logfile
-
+var2=wc -l /var/www/html/EventLog.txt
 #Check if photos grew by 2 in size
-if [ $((var+2)) -eq $varfter ]; then
-    echo "FR6 Passed"
+if [ $var2 -gt $var ]; then
+    echo "FR9 Passed"
 else
-    echo "FR6 Failed"
+    echo "FR9 Failed"
 fi
 echo "------------------------------"
