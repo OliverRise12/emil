a35fdf3606aabae9bd883f071e9cd083ac985e4f
annotater <annotater@drone.com>
2024-05-31 10:42:31 +0200

    test reset and annotater fix

--- projectFiles/annotater.sh
+++ projectFiles/annotater.sh
@@ -32,7 +32,7 @@ while true; do
 		#update json(derulooo) in photo archive 
 		sudo sed -i '$d' $path_to_archive/$pic.json #delete last line from file
 		sudo truncate -s-1 $path_to_archive/$pic.json #write to last line of file
-		sudo echo -e ",\n\t\"Annotation\":{\n\t\t\"Source\": \"Ollama:7b\",\n\t\t\"Test\": \"$annotation\"\n\t}\n}" >> $path_to_archive/$pic.json #pipe data to file
+		sudo echo -e "\n\t\"Annotation\":{\n\t\t\"Source\": \"Ollama:7b\",\n\t\t\"Test\": \"$annotation\"\n\t}\n}" >> $path_to_archive/$pic.json #pipe data to file
 
 		sudo cp "$pic.json" "$path_to_metadata"
 
